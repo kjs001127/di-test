d938ff4c442533a32cc34789180e0a87134e126b
Camel <camel@ip-192-168-3-125.ap-northeast-2.compute.internal>
2022-12-19 17:18:13 +0900

    fix

--- fx/main.go
+++ fx/main.go
@@ -13,44 +13,37 @@ import (
 )
 
 func main() {
+	fx.New(
+		envModule(),
+		commonModule(),
+		fx.Invoke(httpServer),
+	).Run()
+}
 
-	var module fx.Option
+func envModule() (ret fx.Option) {
 	switch os.Getenv("GO_ENV") {
 	case "prod":
-		module = prodModule()
+		ret = prodModule()
 	case "test":
-		module = testModule()
+		ret = testModule()
 	default:
 		panic("unknown env")
 	}
-
-	fx.New(
-		module,
-		commonModule(),
-		fx.Invoke(func(s *http.Server, lc fx.Lifecycle) { // invoke 는 fx.New (Initialize) 시점에 호출됌
-			lc.Append(fx.Hook{ // fx..Hook 을 통해 추가된 lifecycle hook invoke 이후 execution 에서 실행됌
-				OnStart: func(ctx context.Context) error {
-					return startHttpSvr(s)
-				},
-				OnStop: func(ctx context.Context) error {
-					return stopHttpSvr(ctx, s)
-				},
-			})
-		}),
-	).Run()
-
-	fx.Populate()
+	return
 }
 
-func startHttpSvr(s *http.Server) error {
-	fmt.Println("Starting server")
-	go s.ListenAndServe()
-	return nil
-}
-
-func stopHttpSvr(ctx context.Context, s *http.Server) error {
-	fmt.Println("ending server", s)
-	return s.Shutdown(ctx)
+func httpServer(s *http.Server, lc fx.Lifecycle) { // invoke 는 fx.New (Initialize) 시점에 호출됌
+	lc.Append(fx.Hook{ // fx..Hook 을 통해 추가된 lifecycle hook invoke 이후 execution 에서 실행됌
+		OnStart: func(ctx context.Context) error {
+			fmt.Println("Starting server")
+			go s.ListenAndServe()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			fmt.Println("ending server", s)
+			return s.Shutdown(ctx)
+		},
+	})
 }
 
 func testModule() fx.Option {
@@ -78,12 +71,12 @@ func prodModule() fx.Option {
 			fx.Annotate(
 				app.NewHelloHandler,
 				fx.As(new(app.Route)),
-				fx.ResultTags(`group:"route"`),
+				fx.ResultTags(`group:"test"`),
 			),
 			fx.Annotate(
 				app.NewEchoHandler,
 				fx.As(new(app.Route)), // fx.As 로 Route 인터페이스로 바인딩하겠다고 선언, 여러개 선언 가능
-				fx.ResultTags(`group:"route"`),
+				fx.ResultTags(`group:"test"`),
 				fx.ParamTags(`name:"echoPath"`), // fx.ParamTags 로 name 지정
 			),
 			zap.NewProduction,
@@ -96,7 +89,7 @@ func commonModule() fx.Option {
 		app.NewHTTPServer, // 기본적으로는 이렇게 생성자 함수를 제공
 		fx.Annotate( // fx.Annotate 로 부가적인 정보 제공
 			app.NewServeMux,
-			fx.ParamTags(`group:"route"`), // fx.ParamTags 로 group 지정
+			fx.ParamTags(`group:"test"`), // fx.ParamTags 로 group 지정
 			// 파라미터 []Route 에 group:route 로 태깅된 인스턴스들을 주입
 		),
 	))
@@ -105,27 +98,26 @@ func commonModule() fx.Option {
 func main2() {
 	c := dig.New()
 
-	err := c.Provide("db", dig.Name("a"))
+	err := c.Provide(app.NewHelloHandler, dig.As(new(app.Route)))
 	if err != nil {
 		panic(err)
 	}
 
-	err = c.Provide(app.NewHelloHandler, dig.As(new(app.Route)), dig.Group("route"))
+	err = c.Provide(zap.NewDevelopment)
 	if err != nil {
 		panic(err)
 	}
 
-	err = c.Provide(app.NewEchoHandler, dig.As(new(app.Route)), dig.Group("route"))
+	err = c.Provide(app.NewServeMux)
 	if err != nil {
 		panic(err)
 	}
 
-	err = c.Provide(app.NewServeMux, dig.Group("route"))
 	err = c.Invoke(func(r app.Route) {
 		fmt.Print(r.Pattern())
 	})
 	if err != nil {
-		return
+		panic(err)
 	}
 	time.Sleep(time.Hour)
 }
